40721d6909abf92d6d25c041708671421f804d6d
AllenLu27 <47432554+AllenLu27@users.noreply.github.com>
2020-08-21 00:01:04 -0500

    New feature
    Authenticated user can now edit date and time of the meeting

--- src/store/index.js
+++ src/store/index.js
@@ -62,7 +62,8 @@ export const store = new Vuex.Store({
                 description: obj[key].description,
                 imageUrl: obj[key].imageUrl,
                 date: obj[key].date,
-                creatorId: obj[key].creatorId
+                creatorId: obj[key].creatorId,
+                location: obj[key].location
               })
             }
             commit('setLoadedMeetups', meetups)
